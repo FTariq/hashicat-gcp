800610eef58b2aeb394b5999fef08deb1c2c7898
FTariq <farhantariq321@gmail.com>
2021-11-04 13:50:09 +0000

    Resolve module issues

--- vpc.tf
+++ vpc.tf
@@ -1,9 +1,14 @@
-network_name = "farhan-network"
-project_id = var.project
-subnets = [
-  {
-    subnet_name   = "farhan-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+module "vpc" {
+    source  = "terraform-google-modules/network/google"
+    version = "~> 3.0"
+
+    network_name = "farhan-network"
+    project_id = var.project
+    subnets = [
+      {
+        subnet_name   = "farhan-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+      }
+    ]
+}
