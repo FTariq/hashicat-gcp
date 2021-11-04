3a0c0673ab1e200e9ca22c8595fb915b95926e5c
FTariq <farhantariq321@gmail.com>
2021-11-04 14:18:19 +0000

    Add unique networks again

--- vpc.tf
+++ vpc.tf
@@ -2,11 +2,11 @@ module "network" {
   source  = "app.terraform.io/farz-company/network/google"
   version = "4.0.0"
 
-    network_name = "farzy-network1"
+    network_name = "farzy-network2"
     project_id = var.project
     subnets = [
       {
-        subnet_name   = "farzy-subnet1"
+        subnet_name   = "farzy-subnet2"
         subnet_ip     = "10.100.10.0/24"
         subnet_region = var.region
       }
